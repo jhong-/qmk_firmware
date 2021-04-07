7698eb369246d9e463d6e2ad874c4ad9192155f1
Jim Hong <jimqhong@gmail.com>
2021-04-06 19:11:30 -0700

    Added NAV layer

--- keyboards/preonic/keymaps/jhong/keymap.c
+++ keyboards/preonic/keymaps/jhong/keymap.c
@@ -3,6 +3,7 @@
 
 enum preonic_layers {
   _QWERTY,
+  _NAV,
   _LOWER,
   _RAISE,
   _ADJUST
@@ -10,6 +11,7 @@ enum preonic_layers {
 
 enum preonic_keycodes {
   QWERTY = SAFE_RANGE,
+  NAV,
   LOWER,
   RAISE,
   BACKLIT
@@ -34,11 +36,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Enter: Hold - Right Shift, Tap - Enter
  */
 [_QWERTY] = LAYOUT_preonic_grid(
-  KC_GRV,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
-  KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-  LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
-  BACKLIT,        KC_RCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  KC_GRV,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,             KC_6,              KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
+  KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,              KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+  LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,              KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,              KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
+  BACKLIT,        KC_RCTL, KC_LALT, KC_LGUI, LOWER,   LT(_NAV, KC_SPC), LT(_NAV, KC_SPC),  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+),
+
+/* Nav
+ * ,-----------------------------------------------------------------------------------.
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |      |      | BRIU | BRID |      |      | Vol- | Vol+ |      | PSCR |      |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |      | End  | Pg Up| Pg Dn| Home | Left | Down |  Up  |Right |      |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      | Mute |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_NAV] = LAYOUT_preonic_grid(
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, KC_BRIU, KC_BRID, _______, _______, KC_VOLD, KC_VOLU, _______, KC_PSCR, _______,
+  _______, _______, KC_END,  KC_PGUP, KC_PGDN, KC_HOME, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, KC_MUTE, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 /* Lower
