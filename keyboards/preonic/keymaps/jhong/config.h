7318013a081b34415581b14e727a29eb8685f6ac
Jim Hong <jimqhong@gmail.com>
2022-12-12 22:11:47 -0800

    Enabled PERMISSIVE_HOLD
    This commit decreases the TAPPING_TERM to prevent accidentally tapping
    instead of holding.

--- keyboards/preonic/keymaps/jhong/config.h
+++ keyboards/preonic/keymaps/jhong/config.h
@@ -33,6 +33,7 @@
 /* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
 //#define MIDI_TONE_KEYCODE_OCTAVES 2
 
+#define PERMISSIVE_HOLD
 #define TAPPING_TERM 200
 
 /* #define MK_3_SPEED */
